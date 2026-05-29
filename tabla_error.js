605e2cb18fd768c30216d379d0690acb44ea97c3
mildred24pineda <mpinedap3@miumg.edu.gt>
2026-05-29 16:52:28 -0600

    Delete tabla_error.js

--- tabla_error.js
+++
@@ -1,9 +0,0 @@
-class TablaError {
-  constructor() { this.errores = []; }
-  agregar(type, location, description) { this.errores.push({ type, location, description }); }
-  extender(lista = []) { this.errores.push(...lista); }
-  hayErrores() { return this.errores.length > 0; }
-  limpiar() { this.errores = []; }
-  obtener() { return [...this.errores]; }
-}
-module.exports = TablaError;
